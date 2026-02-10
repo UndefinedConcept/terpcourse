7740e11552b906444cd9658c7cba6394997304db
UndefinedConcept <94262481+UndefinedConcept@users.noreply.github.com>
2026-02-10 18:17:17 -0500

    clean installation of svelte with tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,58 +1,9 @@
-<script>
-	import Header from '$lib/header/Header.svelte';
-  import { webVitals } from '$lib/vitals';
-  import { browser } from '$app/env';
-  import { page } from '$app/stores';
-  import '../app.css';
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
 
-  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-
-  $: if (browser && analyticsId) {
-    webVitals({
-      path: $page.url.pathname,
-      params: $page.params,
-      analyticsId
-    })
-  }
+	let { children } = $props();
 </script>
 
-<Header />
-
-<main>
-	<slot />
-</main>
-
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
-
-<style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
-</style>
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
